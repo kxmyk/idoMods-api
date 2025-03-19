e9bd5d54be047dea121a4f1d3637260f0d775ee7
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-03-19 12:02:51 +0100

    [FEATURE] Initial commit & configuration

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+    idoSellAPI: {
+        baseUrl: process.env.IDOSELL_API_BASE_URL || "",
+        apiKey: process.env.IDOSELL_API_KEY || ""
+    },
+    server: {
+        port: process.env.PORT || 3000,
+        basicAuth: {
+            username: process.env.BASIC_AUTH_USER,
+            password: process.env.BASIC_AUTH_PASS
+        }
+    },
+    db: {
+        uri: process.env.MONGO_URI
+    }
+};
